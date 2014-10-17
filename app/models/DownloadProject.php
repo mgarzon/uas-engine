d3685e71f042f9500e14d0d57057af9606d9fc9f
Nabil Maadarani <nabil.maadarani@hotmail.com>
2014-10-17 14:11:48 -0400

    Added unzipping capability.

--- app/models/DownloadProject.php
+++ app/models/DownloadProject.php
@@ -1,8 +1,20 @@
 <?php
   include("Downloader.php");
 
+  // Fetch command-line arguments
   $url = $argv[1];
   $dir = $argv[2];
 
+  // Download project
   $myDownloader = new Downloader($url, $dir);
   $myDownloader->downloadProject();
+
+  // Extract project
+  $file = $dir . "/project.zip";
+  $zip = new ZipArchive;
+  $res = $zip->open($file);
+
+  if($res == TRUE) {
+  	$zip->extractTo($dir);
+  	$zip->close();
+  }
