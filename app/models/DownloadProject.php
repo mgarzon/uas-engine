43b198294db243a6ef9f7dfaa7dc7a7daf234152
Nabil Maadarani <nabil.maadarani@hotmail.com>
2014-10-31 15:05:17 -0400

    UAS Engine now downloads any project on GitHub or Google Code provided the project name only, and automatically umplifies the project

--- app/models/DownloadProject.php
+++ app/models/DownloadProject.php
@@ -1,18 +1,27 @@
 <?php
   include("Downloader.php");
+  include("Umplificator.php");
 
   // Fetch command-line arguments
   $repo = $argv[1];
-  $url = $argv[2];
+  $name = $argv[2];
   $dir = $argv[3];
 
+  // Get project URL
+  if(strcmp($repo, 'gh') == 0) {
+    // GitHub URL
+    $url = "https://github.com/" . $name . "/archive/master.zip";
+  } else if(strcmp($repo, 'gc') == 0) {
+    // Google Code URL
+    $url = "https://code.google.com/p/" . $name;
+  }
+
   // Create Downloader instance
-  $myDownloader = new Downloader($repo, $url, $dir);
+  $myDownloader = new Downloader($repo, $url, $dir, $name);
 
   if(strcmp($myDownloader->getProjectRepo(), 'gh') == 0) {
     // Download project from Github & unzip
     $myDownloader->downloadProjectGithub();
-    echo("The project name is " . $myDownloader->getProjectName() . "\n");
   }
 
   else if(strcmp($myDownloader->getProjectRepo(), 'gc') == 0) {
@@ -23,3 +32,7 @@
   else {
     echo("** ERROR: Repo must be gh for Github or gc for Google Code\n");
   }
+
+  // Umplify project
+   $myUmplificator = new Umplificator($myDownloader->getDestinationDir() . $myDownloader->getProjectName());
+   $myUmplificator->umplifyProject(0);
